6e9782c832337619f6f967f56c04477fa8b9bf12
musaninsopasi <77739510+musaninsopasi@users.noreply.github.com>
2021-10-19 20:51:36 +0300

    Add files via upload

---
+++ mizah.py
@@ -0,0 +1,34 @@
+def add(x, y):
+   return x + y
+def subtract(x, y):
+   return x - y
+def multiply(x, y):
+   return x * y
+def divide(x, y):
+   return x / y
+print("işlem seç.")
+print("1.toplama")
+print("2.çıkarma")
+print("3.çarpma")
+print("4.bölme")
+choice = input("Hangi işlemi yapmak istiyorsun(1/2/3/4): ")
+num1 = float(input("İlk sayıyı gir: "))
+num2 = float(input("İkinici sayıyı gir: "))
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+   if add(num1,num2) == 31:
+     print("DŞSLKFJOIREWOIREWOIREWOIREWOIREWOIREWUHUIRE")
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+   if add(num1,num2)== 31:
+     print("ÇÖFCMGNFLKDJHGLKJRHETIOREHAŞSD")
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+   if add(num1,num2)== 31:
+     print("POEIRUDSFNDÖMGNÖMFDNGFDMG")
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+   if add(num1,num2) == 31:
+     print("ASDŞKJSAHDŞKJLSAHEPOIUWQWQ")
+else:
+   print("Geçersiz Giriş")
