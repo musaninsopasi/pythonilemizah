a6e393420ec3e2c00b34f8737070325244b40728
musaninsopasi <77739510+musaninsopasi@users.noreply.github.com>
2021-12-17 23:03:01 +0300

    Create hesapmakinesi.py

---
+++ hesapmakinesi.py
@@ -0,0 +1,46 @@
+def topla (x, y):
+    return x+y
+
+def cikar (x, y):
+        return x-y
+
+def carp (x, y):
+          return x*y
+            
+def bol (x, y):
+                return x/y
+
+
+
+
+print ('Yapılacak işlemi seçiniz')
+
+print("=================")
+
+print("1.Toplama")
+print("2. Çıkarma")
+print("3. Çarpma")
+print("4. Bölme")
+
+secim = input("Seçiminiz (1/2/3/4): ")
+
+ 
+
+
+sayi1 = int(input("1. Sayı: "))
+sayi2 = int(input("2. Sayı: "))
+
+if secim == '1' :
+    print(sayi1, "+",sayi2, "=", topla(sayi1, sayi2))
+
+elif secim == '2' : 
+    print(sayi1, "-",sayi2, "=", cikar(sayi1, sayi2))
+
+elif secim == '3' :
+    print(sayi1, "*", sayi2, "=", carp(sayi1,sayi2))
+
+elif secim == '4' :
+    print(sayi1,"/", sayi2, "=", bol(sayi1,sayi2))
+
+else :
+    print("Geçersiz Giriş!") 
